ce413ce29a23e831ccd2acd5723618afd9ea028e
Francesco Piraneo G <francesco@piraneo-canepa.ch>
2014-02-13 17:09:21 +0100

    Added file delete hooks
    When a file is deleted cleanup of thumbnail and DB entries

--- oclife/_NOTES/notes.php
+++ oclife/_NOTES/notes.php
@@ -16,3 +16,22 @@ $user = OCP\User::getUser();
 $myDir = \OC_User::getHome($user);
 
 // On javascript: t('oclife', 'Informations') to localize textes
+
+/**
+* Send json error msg
+*
+* Return a json error message with optional extra data for
+* error message or app specific data.
+*
+* Example use:
+*
+*     $id = [some value]
+*     OCP\JSON::error(array('data':array('message':'An error happened', 'id': $id)));
+*
+* Will return the json formatted string:
+*
+*     {"status":"error","data":{"message":"An error happened", "id":[some value]}}
+*
+* @param array $data The data to use
+* @return string json formatted error string.
+*/

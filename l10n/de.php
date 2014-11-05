243cde34c16f18e197db7d3d2c0bd9f6db7167a4
fcturner <fcturner@users.noreply.github.com>
2014-11-05 08:37:57 +0100

    Create de.php
    German translation, as proposed by go4oc on app.owncloud.

---
+++ l10n/de.php
@@ -0,0 +1,89 @@
+<?php
+$TRANSLATIONS = array(
+// Admin setup
+// - php and templates
+'ImageMagick is loaded and ready to be used' => 'ImageMagick ist vorhanden und kann genutzt werden.',
+'ImageMagick is not loaded: Please refers to php manual.' => 'ImageMagick fehlt. Beachten Sie Hinweise im PHP Handbuch!',
+'Only admins can edit tags' => 'Nur Administratoren können Tags editieren.',
+'Use ImageMagick instead of GD (More image formats handled)' => 'Verwenden Sie statt GD ImageMagick. ImageMagick verarbeitet mehr Bildformate.',
+
+// - JavaScript
+'Settings not saved! Data base error!', 'Einstelleungen wurden nicht gespeichert! Datenbankfehler!',
+'Settings not saved! Communication error!', 'Einstellungen wurden nicht gespeichert! Kommunikationsfehler!',
+
+// Main app
+// - php and templates
+'Actual tags' => 'Vorhandene Tags',
+'Expand all' => 'Anzeigen',
+'Collapse all' => 'Verstecken',
+'Rename tag' => 'Tag umbenennen',
+'Rename the tag and confirm.' => 'Tag umbenennen und bestätigen',
+'Create a new tag' => 'Tag erstellen.',
+'Insert the new tag and confirm.' => 'Tag einfügen und bestätigen',
+'Delete tag' => 'Tag löschen',
+'Really delete the tag:' => 'Tag wirklich löschen:',
+'NOTE: ' => 'Hinweis: ',
+'Also child tags will be removed!' => 'Zugeordnete Tags werden auch gelöscht!',
+'Where is this file?' => 'Wo befindet sich die Datei?',
+'You can find this file here:' => 'Die Datei befindet sich hier:',
+'Associated files' => 'Zugehörige Dateien',
+'Select one or more tags to view the associated files.' => 'Einen oder mehrere Tags wählen, um zugehörige Dateien anzuzeigen.',
+
+// - JavaScript
+'Unable to get files list!' => 'Dateiliste kann nicht erstellt werden!',
+'Tag moved successfully!' => 'Tag erfolgreich verschoben!',
+'Tag not moved! Data base error!' => 'Tag nicht verschoben! Datenbankfehler!',
+'Tag not moved! Ajax error!' => 'Tag nicht verschoben! Ajaxfehler!',
+'Rename' => 'Umbenennen',
+'New' => 'Neu',
+'Delete' => 'Löschen',
+'Nothing possible' => 'Nichts möglich',
+'Editing of Root node not allowed!' => 'Editieren des Hauptknotens nicht erlaubt!',
+'Deleting of Root node not allowed!' => 'Löschen des Hauptknotens nicht erlaubt!',
+'Lenght must be between ' => 'Möglicher Längenbereich: ',
+'Cancel' => 'Abbrechen',
+'Rename done!' => 'Umbenennen erfolgreich!',
+'Unable to rename! Data base error!' => 'Umbenennen unmöglich! Datenbankfehler!',
+'Unable to rename! Ajax error!' => 'Umbenennen unmöglich! Ajaxfehler',
+'Tag created successfully!' => 'Tag erstellt!',
+'Unable to create tag! Data base error!' => 'Tag nicht erstellt! Datenbankfehler!',
+'Unable to create tag! Ajax error!' => 'Tag nicht erstellt! Ajaxfehler!',
+'Operation canceled: No deletion occurred!' => 'Ausführung ohne Löschen abgebrochen!',
+'Invalid tag number! Nothing done!' => 'Ungültige Tag Nummer! Keine Ãnderungen vorgenommen!',
+'Tag removed successfully!' => 'Tag erfolgreich gelöscht!',
+'Tag not removed! Data base error!' => 'Tag nicht gelöscht! Datenbankfehler!',
+'Tags not removed! Ajax error!' => 'Tag nicht gelöscht! Ajaxfehler!',
+'Confirm' => 'Bestätigen',
+
+// Extended info popup
+// - JavaScript
+'Tag selected files' => '',
+'Unable to get user\'s priviledge.' => 'Ermittlung der Benutzerrechte nicht möglich.',
+'Informations' => 'Informationen',
+'Enter tags here' => 'Tag hier eingeben',
+'File(s) where the tags will be applied' => 'Dateien, auf welche die Tags angewendet werden.',
+'Tags common to all files' => 'Tags auf alle Dateien anwenden:',
+'Tags for the selected file' => 'Tags auf ausgewählte Datei anwenden:',
+'The tag "' => 'Tag "',
+'" doesn\'t exist; would you like to create a new one?' => '" nicht vorhanden! Erstellen?',
+'Unable to create the tag! Ajax error.' => 'Tag erstellen nicht möglich! Ajaxfehler!',
+'Unable to add the tag! Ajax error.' => 'Tag hinzufügen nicht möglich! Ajaxfehler!',
+'Unable to remove the tag! Data base error.' => 'Tag löschen nicht möglich! Datenbankfehler!',
+'Unable to remove the tag! Ajax error.' => 'Tag löschen nicht möglich! Ajaxfehler!',
+'Unable to get the tags! Ajax error.' => 'Tags ermitteln nicht möglich! Ajaxfehler!',
+'Unable to retrieve informations on this file! Ajax error!' => 'Ermitteln von Informationen für diese Datei nicht möglich! Ajaxfehler!',
+'Unable to get actual tags for this document! Ajax error!' => 'Ermitteln der Tags für diese Datei nicht möglich! Ajaxfehler!',
+'All selected files' => 'Alle ausgewählten Dateien',
+'Informations on' => 'Informationen über',
+'Informations' => 'Informationen',
+
+// getFileInfo.php
+'Multiple files selected' => 'Mehrfach ausgewählte Dateien',
+'File name' => 'Dateiname: ',
+'Size' => 'Dateigrösse: ',
+'When added' => 'Hinzugefügt am: ',
+'Encrypted? ' => 'Verschlüsselt: ',
+'Yes' => 'JA',
+'No' => 'NEIN',
+'Unencrypted size' => 'Dateigrösse unverschlüsselt: '
+);

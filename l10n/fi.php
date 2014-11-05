360b2871f9783cf3f3fb2d7c8610e3523bc50965
Francesco Piraneo G. <francesco@piraneo-canepa.ch>
2014-11-05 07:44:22 +0100

    Create fi.php
    Added Finnish translations

---
+++ l10n/fi.php
@@ -0,0 +1,132 @@
+<?php
+/*
+* Copyright 2014 by Francesco PIRANEO G. (fpiraneo@gmail.com)
+* Translations in Finnish kindly provided by Tomas Nybondas, Finland (nybondasto@gmail.com)
+*
+* This file is part of oclife.
+*
+* oclife is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* oclife is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with oclife. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+$TRANSLATIONS = array(
+    // Admin setup
+    // - php and templates
+    'ImageMagick is loaded and ready to be used' => 'ImageMagick on ladattu ja valmiina käyttöön',
+    'ImageMagick is not loaded: Please refers to php manual.' => 'ImageMagickia ei ole ladattu: tarkista php ohje.',
+    'Only admins can edit tags' => 'Vain ylläpitäjät voivat muokata tageja',
+    'Use ImageMagick instead of GD (More image formats handled)' => 'Käytä ImageMagick:ia GD:n sijaan (useampia kuvaformaatteja käytössä)',
+
+    // - JavaScript
+    'Settings not saved! Data base error!', 'Asetusten tallennus ei onnistunut! Tietokantavirhe!',
+    'Settings not saved! Communication error!', 'Asetusten tallennus ei onnistunut! Yhteysvirhe!',
+    
+    // Main app
+    // - php and templates
+    'Actual tags' => 'Varsinaiset tagit',
+    'Expand all' => 'Laajenna kaikki',
+    'Collapse all' => 'Supista kaikki',
+    'Rename tag' => 'Nimeä tagi uudelleen',
+    'Rename the tag and confirm.' => 'Nimeä tagi uudelleen ja vahvista.',
+    'Create a new tag' => 'Luo uusi tagi',
+    'Insert the new tag and confirm.' => 'Syötä uusi tagi ja vahvista.',
+    'Delete tag' => 'Poista tagi',
+    'Really delete the tag:' => 'Haluatko todella poistaa tagin:',
+    'NOTE: ' => 'HUOM: ',
+    'Also child tags will be removed!' => 'Myös kaikki alitagit poistetaan!',
+    'Where is this file?' => 'Missä tiedosto sijaitsee?',
+    'You can find this file here:' => 'Tiedosto löytyy tästä:',
+    'Associated files' => 'Liitetyt tiedostot',
+    'Select one or more tags to view the associated files.' => 'Valitse yksi tai useampia tageja liitettyjen tiedostojen näyttämiseksi.',
+    'Not set' => 'Ei asetettu',
+    'Owner:' => 'Omistaja:',
+    'Read only' => 'Vain luku',
+    'Can modify' => 'Saa muokata',
+    'None' => 'Ei mitään',
+    'Group:' => 'Ryhmä:',
+    'All:' => 'Kaikki:',
+    'Image preview' => 'Kuvan esikatselu',
+    
+    // - JavaScript
+    'Unable to get files list!' => 'Tiedostoluetteloa ei ole saatavilla!',
+    'Tag moved successfully!' => 'Tagin siirto OK!',
+    'Tag not moved! Data base error!' => 'Tagia ei siirretty! Tietokantavirhe!',
+    'Tag not moved! Ajax error!' => 'Tagia ei siirretty! Ajax-virhe!',
+    'Rename' => 'Nimeä uudelleen',
+    'New' => 'Uusi',
+    'Delete' => 'Poista',
+    'Nothing possible' => 'Ei mitään',
+    'Editing of Root node not allowed!' => 'Juuritagin muokkaus on kielletty!',
+    'Deleting of Root node not allowed!' => 'Juuritagin poistaminen on kielletty!',
+    'Lenght must be between ' => 'Pituuden tulee olla välillä',
+    'Cancel' => 'Peruuta',
+    'Rename done!' => 'Nimen vaihtaminen valmis!',
+    'Unable to rename! Data base error!' => 'Nimen vaihtaminen ei onnistu! Tietokantavirhe!',
+    'Unable to rename! Ajax error!' => 'Nimen vaihtaminen ei onnistu! Ajax-virhe!',
+    'Tag created successfully!' => 'Tagin luonti OK!',
+    'Unable to create tag! Data base error!' => 'Tagin luonti ei onnistu! Tietokantavirhe!',
+    'Unable to create tag! Ajax error!' => 'Tagin luonti ei onnistu! Ajax-virhe!',
+    'Operation canceled: No deletion occurred!' => 'Operaatio peruutettu: mitään ei poistettu!',
+    'Invalid tag number! Nothing done!' => 'Virheellinen tagin numero! Toiminto peruttu.',
+    'Tag removed successfully!' => 'Tagi poistettu!',
+    'Tag not removed! Data base error!' => 'Tagin poistaminen ei onnistu! Tietokantavirhe',
+    'Tags not removed! Ajax error!' => 'Tagin poistaminen ei onnistu! Ajax-virhe!',
+    'Confirm' => 'Vahvista',
+    'Priviledges' => 'Oikeudet',
+    'User - Readonly' => 'Käyttäjä - vain luku',
+    'User' => 'Käyttäjä',
+    'Group - Readonly' => 'Ryhmä - vain luku',
+    'Group' => 'Ryhmä',
+    'Global - Readonly' => 'Kaikki - vain luku',
+    'Global' => 'Kaikki',
+    'Priviledge changed successfully!' => 'Oikeuden muuttaminen OK!',
+    'Priviledge not changed! Data base error!' => 'Oikeuden muuttaminen ei onnistu! Tietokantavirhe',
+    'Priviledge not changed! Ajax error!' => 'Oikeuden muuttaminen ei onnistu! Ajax-virhe!',
+    'Priviledge not changed! Permission denied!' => 'Oikeuden muuttaminen ei onnistu! Oikeudet eivät riitä!',
+    'Owner changed successfully!' => 'Omistajan muuttaminen OK!',
+    'Owner not changed! Permission denied!' => 'Omistajan muuttaminen ei onnistu! Oikeudet eivät riitä!',
+    'Owner not changed! Data base error!' => 'Omistajan muuttaminen ei onnistu! Tietokantavirhe!!',
+    'Owner not changed! Ajax error!' => 'Omistajan muuttaminen ei onnistu! Ajax-virhe!',
+    
+    // Extended info popup
+    // - JavaScript
+    'Tag selected files' => 'Lisää tagi valittuihin tiedostoihin',
+    'Unable to get user\'s priviledge.' => 'Käyttäjän oikeuksia ei ole saatavilla.',
+    'Informations' => 'Tietoja',
+    'Enter tags here' => 'Syötä tagit tähän',
+    'File(s) where the tags will be applied' => 'Tiedostot, joihin tagit liitetään',
+    'Tags common to all files' => 'Kaikille tiedostoille yhteiset tagit',
+    'Tags for the selected file' => 'Valitun tiedoston tagit',
+    'The tag "' => 'Tagia "',
+    '" doesn\'t exist; would you like to create a new one?' => '" ei ole? Haluatko luoda uuden?',
+    'Unable to create the tag! Ajax error.' => 'Tagin luominen ei onnistu! Ajax-virhe!',
+    'Unable to add the tag! Ajax error.' => 'Tagin lisääminen ei onnistu! Ajax-virhe!',
+    'Unable to remove the tag! Data base error.' => 'Tagin poistaminen ei onnistu! Tietokantavirhe!',
+    'Unable to remove the tag! Ajax error.' => 'Tagin poistaminen ei onnistu! Ajax-virhe!',
+    'Unable to get the tags! Ajax error.' => 'Tagien tuonti ei onnistu! Ajax-virhe!',
+    'Unable to retrieve informations on this file! Ajax error!' => 'Tiedoston tietojen tuominen ei onnistu! Ajax-virhe!',
+    'Unable to get actual tags for this document! Ajax error!' => 'Tiedoston varsinaisten tagien tuominen ei onnistu! Ajax-virhe!',
+    'All selected files' => 'Kaikki valitut tiedostot',
+    'Informations on' => 'Tiedoston tiedot',
+    'Informations' => 'Tiedot',
+    
+    // getFileInfo.php
+    'Multiple files selected' => 'Useita tiedostoja valittu',
+    'File name' => 'Tiedoston nimi',
+    'Size' => 'Koko',
+    'When added' => 'Milloin lisätty',
+    'Encrypted? ' => 'Onko salattu? ',
+    'Yes' => 'Kyllä',
+    'No' => 'Ei',
+    'Unencrypted size' => 'Salaamattoman koko'
+);
